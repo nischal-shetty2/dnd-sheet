86b63b2b2943e078c06b46bc1bde259b60772ffc
Nischal Shetty <nischal2004coc@gmail.com>
2025-02-24 22:20:00 +0530

    fixed dark mode text

--- components/question-item.tsx
+++ components/question-item.tsx
@@ -96,11 +96,11 @@ export function QuestionItem({ topicId, question }: QuestionItemProps) {
   const getDifficultyColor = (difficulty: string) => {
     switch (difficulty.toLowerCase()) {
       case "easy":
-        return "bg-green-50 ";
+        return "bg-green-50 dark:bg-green-900 text-black dark:text-white";
       case "medium":
-        return "bg-yellow-50 ";
+        return "bg-yellow-50 dark:bg-yellow-900 text-black dark:text-white";
       case "hard":
-        return "bg-red-50 ";
+        return "bg-red-50 dark:bg-red-900 text-black dark:text-white";
       default:
         return "";
     }
@@ -126,7 +126,7 @@ export function QuestionItem({ topicId, question }: QuestionItemProps) {
               <GripVertical className="h-4 w-4" />
             </Button>
           </TooltipTrigger>
-          <TooltipContent className=" text-xs bg-gray-200 rounded-xl mb-2 px-2 py-1">
+          <TooltipContent className=" text-xs bg-gray-200 rounded-xl dark:text-black mb-2 px-2 py-1">
             Drag to reorder
           </TooltipContent>
         </Tooltip>
@@ -182,15 +182,29 @@ export function QuestionItem({ topicId, question }: QuestionItemProps) {
             <TooltipTrigger asChild>
               <Button
                 onClick={() =>
-                  (window.location.href = question.videoUrl || "/")
+                  window.open(
+                    "https://www.youtube.com/watch?v=10TSKjoglc0",
+                    "_blank"
+                  )
                 }
                 variant="ghost"
                 size="icon"
                 className="shrink-0">
-                <Youtube className="h-4 w-4 text-red-500" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlSpace="preserve"
+                  viewBox="0 0 16 16"
+                  id="youtube">
+                  <g fillRule="evenodd" clipRule="evenodd">
+                    <path
+                      fill="#F44336"
+                      d="M15.32 4.06c-.434-.772-.905-.914-1.864-.968C12.498 3.027 10.089 3 8.002 3c-2.091 0-4.501.027-5.458.091-.957.055-1.429.196-1.867.969C.23 4.831 0 6.159 0 8.497v.008c0 2.328.23 3.666.677 4.429.438.772.909.912 1.866.977.958.056 3.368.089 5.459.089 2.087 0 4.496-.033 5.455-.088.959-.065 1.43-.205 1.864-.977.451-.763.679-2.101.679-4.429v-.008c0-2.339-.228-3.667-.68-4.438z"></path>
+                    <path fill="#FAFAFA" d="M6 11.5v-6l5 3z"></path>
+                  </g>
+                </svg>
               </Button>
             </TooltipTrigger>
-            <TooltipContent className=" text-xs bg-gray-200 rounded-xl mb-2 px-2 py-1">
+            <TooltipContent className=" text-xs bg-gray-200 rounded-xl dark:text-black mb-2 px-2 py-1">
               Watch solution
             </TooltipContent>
           </Tooltip>
@@ -201,7 +215,7 @@ export function QuestionItem({ topicId, question }: QuestionItemProps) {
                 <Star className="h-4 w-4 text-yellow-500" />
               </Button>
             </TooltipTrigger>
-            <TooltipContent className=" text-xs bg-gray-200 rounded-xl mb-2 px-2 py-1">
+            <TooltipContent className=" text-xs bg-gray-200 rounded-xl dark:text-black mb-2 px-2 py-1">
               Add to favorites
             </TooltipContent>
           </Tooltip>
@@ -216,7 +230,7 @@ export function QuestionItem({ topicId, question }: QuestionItemProps) {
                 <Edit2Icon className="h-4 w-4" />
               </Button>
             </TooltipTrigger>
-            <TooltipContent className=" text-xs bg-gray-200 rounded-xl mb-2 px-2 py-1">
+            <TooltipContent className=" text-xs bg-gray-200 rounded-xl dark:text-black mb-2 px-2 py-1">
               Edit question
             </TooltipContent>
           </Tooltip>
@@ -231,7 +245,7 @@ export function QuestionItem({ topicId, question }: QuestionItemProps) {
                 <Trash className="h-4 w-4" />
               </Button>
             </TooltipTrigger>
-            <TooltipContent className=" text-xs bg-gray-200 rounded-xl mb-2 px-2 py-1">
+            <TooltipContent className=" text-xs bg-gray-200 rounded-xl dark:text-black mb-2 px-2 py-1">
               Delete question
             </TooltipContent>
           </Tooltip>
